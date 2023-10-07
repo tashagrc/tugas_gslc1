f1dd8d28b51623f2ea6ea6ebd463680ec58ab8a4
Natasha Grace <natashagradika@gmail.com>
2023-10-07 21:55:02 +0700

    finish

--- app/src/main/java/com/example/tugas_gslc1/MainActivity.java
+++ app/src/main/java/com/example/tugas_gslc1/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.tugas_gslc1;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goToMenuButton = findViewById(R.id.goToMenuButton);
+        goToMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/tugas_gslc1/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.example.tugas_gslc1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
